59f97ba69f0286bdf0a4570c2dd8023bdc5b52c0
zf2169 <zf2169@columbia.edu>
2017-03-22 12:14:26 -0400

    pca

--- lib/pca.R
+++ lib/pca.R
@@ -3,12 +3,12 @@
 # Combined with cross-validation step to avoid overfitting (set training and test data for PCA process)
 
 # First read in 2000x10000 features extracted from SIFT
-feature100000<-read.csv("../data/histsall 10000.csv", header = F)
-feature100000<-t(feature100000)
+feature10000<-read.csv("../data/histsall 10000.csv", header = F)
+feature10000<-t(feature10000)
 
 # prcomp can handle the high dimension problem that the number of predictors are much larger than the number of
 # observations, and it would get as much principal components. In this case, we would get 2000. Thus, we need not
 # to select the suitable number of principal components.
-pca <- prcomp(feature100000, scale = T)
+pca <- prcomp(feature10000, scale = T)
 rot<- data.frame(pca$rotation)
 # write.csv(rot, "../output/rot.csv")
