200e0ede217d060b08100d0bc0a77a40de23686d
Zishuo (Jason)  Li <zl2528@columbia.edu>
2017-03-22 15:24:34 -0400

    Add files via upload

--- lib/knn     ads proj 3 zl2528.R
+++ lib/knn     ads proj 3 zl2528.R
@@ -1,47 +1,38 @@
 library('FNN')
+# read data
+label_data <- read.csv("../data/labels.csv",header = T)
+label=factor(label_data[,1])
+feature10000<-read.csv("../data/histsall 10000.csv", header = F)
+feature10000=t(feature10000)
+# accroding to pca get new feature data
 
+pca <- prcomp(feature10000, scale = T)
+rot<- data.frame(pca$rotation)
+rot <- as.matrix(rot)
+feature_data <- feature10000%*%rot
+dim(feature_data)
 
-path='/training_data/training_data/sift_features'
-path.folder<- paste(getwd(),path,sep = '')
-feature_data=read.csv(paste(path.folder,'/sift_features.csv',sep = ''),header = T)
-feature_data=t(feature_data)
-
-path2='/training_data/training_data/'
-path2.folder=paste(getwd(),path2,sep = '')
-label_data=read.csv(paste(path2.folder,'labels.csv',sep = ''),header = T)
-dim(label_data)
-
-data_matrix=cbind(label_data,feature_data)
-
-
-dim(data_matrix)
-
-predict_rate=c()
-
-for (k in 1:10){
-
+# this part generate train data and test data
 n=2000
 sampe_index=sample(1:n,n/5)
-training=data_matrix[-sampe_index,]
-test=data_matrix[sampe_index,]
-dim(test)
+train_data=feature_data[-sampe_index,]
+train_label <-label[-sampe_index] 
+test_data <- feature_data[sampe_index,]
+test_label <-label[sampe_index]
 
-cl=factor(training[,1])
-knn_result=knn(training[,-1],test[,-1],cl,k=1,prob = T)
-predict=sum(knn_result==test[,1])/length(test[,1])
-predict_rate=c(predict_rate,predict)
-table(knn_result,test[,1])
+# this part is the function defined and it takes 4 variables train_data, train_label, test_data, k
+knn_predict <- function(train_data,train_label,test_data,k=1){
+  knn_result=knn(train_data,test_data,cl=train_label,k=k,prob = T)
+  return(knn_result)}
 
-}
+knn_predict(train_data,train_label,test_data)
 
-
-mean(predict_rate)
-
-
-
-
-
-
+sum(knn_result==test_label)/length(test_label)
+#following might not useful
+#mean(predict_rate)
+#predict=sum(knn_result==test[,1])/length(test[,1])
+#predict_rate=c(predict_rate,predict)
+#table(knn_result,test[,1])
 
 
 
