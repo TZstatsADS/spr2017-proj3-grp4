e46f4acbad1705f9cb5fa0dccbaa0d6dc2cda9ab
yhuang2017 <yh2873@columbia.edu>
2017-03-22 19:39:32 -0400

    aaa

--- lib/train.baseline.r
+++ lib/train.baseline.r
@@ -1,13 +1,3 @@
-packages.used=c("gbm", "caret")
-
-# check packages that need to be installed.
-packages.needed=setdiff(packages.used, 
-                        intersect(installed.packages()[,1], 
-                                  packages.used))
-# install additional packages
-if(length(packages.needed)>0){
-  install.packages(packages.needed, dependencies = TRUE)
-}
 
 
 library(gbm)
@@ -18,5 +8,5 @@ train.baseline=function(train.data)
 gbm1=gbm(y~.,data=train.data,dist="adaboost",n.tree = 400,shrinkage=0.1)
 n=gbm.perf(gbm1)
 gbm2=gbm(y~.,data=train.data,dist="adaboost",n.tree = n,shrinkage=0.1)
-return(list(gbm=gbm2,n=n))
+return(list(gbm=gbm1,n=n))
 }
