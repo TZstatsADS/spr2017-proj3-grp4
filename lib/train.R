c082f28386dbc9176687ac700823bf9856252708
yhuang2017 <yh2873@columbia.edu>
2017-03-22 21:02:24 -0400

    update

--- lib/train.R
+++ lib/train.R
@@ -4,15 +4,16 @@ library(caret)
 library(DMwR)
 library(nnet)
 library(randomForest)
+library(e1071)
 
 ############ gbm ######################
 
 train.baseline=function(train.data)
 {
-  gbm1=gbm(y~.,data=train.data,dist="adaboost",n.tree = 800,shrinkage=0.1)
+  gbm1=gbm(y~.,data=train.data,dist="adaboost",n.tree = 400,shrinkage=0.1)
   n=gbm.perf(gbm1)
-  gbm2=gbm(y~.,data=train.data,dist="adaboost",n.tree = n,shrinkage=0.1)
-  return(list(gbm=gbm2,n=n))
+
+  return(list(gbm=gbm1,n=n))
 }
 
 ############ BP network ######################
